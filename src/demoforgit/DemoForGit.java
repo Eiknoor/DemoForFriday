520cdda13eb6a8d4bf3c58772b9602c4884d4ae9
Eiknoor <batraei@sheridancollege.ca>
2025-01-17 12:22:28 -0500

    making a change

--- src/demoforgit/DemoForGit.java
+++ src/demoforgit/DemoForGit.java
@@ -15,9 +15,10 @@ public class DemoForGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        System.out.println("Hey its Eiknoor!");
          //Adding a comment
          //making another change
+         
     }
     
 }
